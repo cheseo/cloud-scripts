3b66763764152b2ff8b8749c830a8f0985f5d0f5
Mr. Me <me@example.tld>
2025-06-26 12:12:29 +0545

    aws cli wip

---
+++ week-3/cloud-init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+config(){
+    cat
+} <<EOF
+server{
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name _;
+	root /var/www/mysite;
+}
+EOF
+
+html(){
+    cat
+}<<EOF
+<!DOCTYPE html>
+<html>
+<head><title>My Static Site!</title>
+</head>
+<body>
+<h1>Static Site!</h1>
+<p>This site has been served using cloud-init.sh !! </p>
+</body>
+</html>
+EOF
+
+apt update
+apt install -y nginx
+
+mkdir /var/www/mysite
+html > /var/www/mysite/index.html
+
+
+config > /etc/nginx/sites-enabled/mysite
+rm /etc/nginx/sites-enabled/default
