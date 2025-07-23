0fdad108d84dcf47e9beba6f75055d3bf0781572
boink <boink@example.com>
2025-07-23 18:59:13 +0545

    add week 5

---
+++ week-5/cloud-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+config(){
+	cat
+}<<EOF
+ec2.ashwink.com.np {
+	reverse_proxy localhost:8080
+}
+
+EOF
+
+wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ >  /etc/apt/sources.list.d/jenkins.list
+
+apt update
+apt install -y jenkins openjdk-21-jre docker.io caddy
+
+usermod -a -G docker jenkins
+
+config > /etc/caddy/Caddyfile
+
+reboot
